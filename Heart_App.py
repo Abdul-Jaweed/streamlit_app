693ba342f4ed07540cec60242c702c227c470b73
Abdul Jaweed <114526738+Abdul-Jaweed@users.noreply.github.com>
2023-03-16 14:19:18 +0530

    Heart App pages fixed

--- Heart_App.py
+++ Heart_App.py
@@ -1,9 +1,47 @@
+# import streamlit as st
+# import os
+# from matplotlib import image
+
+
+# st.title(":red[Heart Disease Prediction APP]")
+
+# st.image("heart.jpg")
+# st.text("This is an application to test presence and absence of heart disease")
+
+
+
 import streamlit as st
 import os
-from matplotlib import image
 
 
-st.title(":red[Heart Disease Prediction APP]")
 
-st.image("heart.jpg")
-st.text("This is an application to test presence and absence of heart disease")
+def space():
+    st.markdown("<br>", unsafe_allow_html=True)
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="😎",
+)
+
+
+st.write("# Heya Everyone ! 👋")
+space()
+space()
+space()
+
+st.markdown("# :red[Masna Ashraf]")
+st.subheader(" :blue[Data Scientist]")
+space()
+
+space()
+
+st.markdown(
+    """
+    #### As a junior data scientist with a Master's degree in Computer Science and more than 2 years of teaching experience, I bring a unique combination of technical expertise and communication skills to the field. My passion for data science and analytics has been fueled by my love for problem-solving and a deep curiosity about the world around us.
+    """
+)
+space()
+space()
+st.write("## Connect me on Linkedin [link](https://www.linkedin.com/in/masna-ashraf-4952461b7/)")
+st.write("## Follow me on Github [link](https://github.com/masnaashraf)")
+st.write("## Mail me on masnaashraf@gmail.com")
